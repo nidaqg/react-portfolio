be9b01ce8a36ddf966c700a4a9a4a9797ea151a9
nqg <nidaghuman@gmail.com>
2021-05-10 13:46:35 -0400

    added hover to profile picture, added logo to header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import "./style.css";
+import logo from "../../images/logo.png";
 
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark myColors">
             <section className="container-fluid">
-              <Link className="navbar-brand" to="/">Nida Ghuman</Link>
+              <Link className="navbar-brand" to="/"><img src={logo} alt="logo" className="rounded-circle w-25"/></Link>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavLinks" aria-controls="navbarNavLinks" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
